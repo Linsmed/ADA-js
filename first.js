f58b4955ee9a8c0f2809c304786269a7449167b6
Ejibode <iejibode@gmail.com>
2022-12-09 20:08:10 +0100

    my latest commit

---
+++ first.js
@@ -0,0 +1,13 @@
+/*
+console.log("Hello World!");
+console.log("hi");
+prompt("input your age");
+alert("who are you");
+*/
+
+// week 11 assigment
+let userName;
+let age;
+const userInfo = prompt("enter your userName ");
+const userFurtherInfo = prompt("enter your age");
+alert(`Welcome ${userInfo}, you are  ${userFurtherInfo} years old.`);
