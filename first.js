388199537c8c6354ee9eb1492df0f037c2568e65
Ejibode <iejibode@gmail.com>
2022-12-10 13:29:14 +0100

    my ada js first class

--- first.js
+++ first.js
@@ -13,4 +13,4 @@ const userFurtherInfo = prompt("enter your age");
 //template literals or template strings method
 alert(`Welcome ${userInfo}, you are  ${userFurtherInfo} years old.`);
 //this state,ent writes directly on the page rather than console using document.write
-document.write(`Welcome ${userInfo}, you are ${userFurtherInfo} years old`);
+document.write(`Welcome ${userInfo}, you are ${userFurtherInfo} years old.`);
